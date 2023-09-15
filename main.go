e99b7cf57b507c7b1b49259e0c51fa3924703e62
Frank Mayer <git@frank-mayer.io>
2023-09-15 19:20:17 +0200

    wait for ack

--- main.go
+++ main.go
@@ -16,12 +16,14 @@ const (
 type ScreemServer struct {
 	hostConn  net.Conn
 	guestConn net.Conn
+	ack       chan bool
 }
 
 func main() {
 	ss := &ScreemServer{
 		hostConn:  nil,
 		guestConn: nil,
+		ack:       make(chan bool),
 	}
 	ss.start()
 	select {}
@@ -33,16 +35,16 @@ func (self *ScreemServer) start() {
 }
 
 func (self *ScreemServer) waitForHost() {
-	hostLn, err := net.Listen("tcp", serverAddr)
+	hostLn, err := net.Listen("tcp", ":5001")
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	fmt.Println("Connect your host to", hostLn.Addr().String())
 
 	for {
 		conn, err := hostLn.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Panicln(err)
 		}
 		fmt.Println("Host connected")
 		self.hostConn = conn
@@ -51,19 +53,20 @@ func (self *ScreemServer) waitForHost() {
 }
 
 func (self *ScreemServer) waitForGuest() {
-	guestLn, err := net.Listen("tcp", serverAddr)
+	guestLn, err := net.Listen("tcp", ":5002")
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	fmt.Println("Connect your guest to", guestLn.Addr().String())
 
 	for {
 		conn, err := guestLn.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Panicln(err)
 		}
 		fmt.Println("Guest connected")
 		self.guestConn = conn
+		go self.ackLoop()
 	}
 }
 
@@ -78,6 +81,8 @@ func (self *ScreemServer) readLoop() {
 		if self.guestConn == nil {
 			// discard the data
 			io.CopyN(io.Discard, self.hostConn, size)
+			// send OK to host
+			binary.Write(self.hostConn, binary.BigEndian, true)
 			continue
 		}
 
@@ -87,7 +92,23 @@ func (self *ScreemServer) readLoop() {
 		// send data to guest
 		_, err := io.CopyN(self.guestConn, self.hostConn, size)
 		if err != nil {
-			log.Fatal(err)
+			log.Panicln(err)
+		}
+
+		// await ack from guest
+		<-self.ack
+
+		// send OK to host
+		binary.Write(self.hostConn, binary.BigEndian, true)
+	}
+}
+
+func (self *ScreemServer) ackLoop() {
+	for {
+		var ack bool
+		binary.Read(self.guestConn, binary.BigEndian, &ack)
+		if ack {
+			self.ack <- true
 		}
 	}
 }
